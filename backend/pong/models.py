6390eb46efd2f0cb2ab0b2930daac657cdf5f39d
Andreia Campos <andreiacampos98.15@gmail.com>
2024-07-07 18:53:00 +0100

    Code: added constraints for enum types.

--- backend/pong/models.py
+++ backend/pong/models.py
@@ -2,30 +2,6 @@ from django.db import models
 from django.core.exceptions import ValidationError
 
 
-class NotificationType(models.TextChoices):
-    FRIEND_REQUEST = "Friend Request"
-    ACCEPTED_FRIEND_REQUEST = "Accepted Friend Request"
-
-class NotificationStatus(models.TextChoices):
-    PENDING = "Pending"
-    READ = "Read"
-
-class TournamentStatus(models.TextChoices):
-    OPEN = "Open"
-    ONGOING = "Ongoing"
-    FINISHED = "Finished"
-
-class TournamentPhase(models.TextChoices):
-    LAST16 = "Last 16"
-    QUARTERFINAL = "Quarter-final"
-    SEMIFINAL = "Semi-final"
-    FINAL = "Final"
-
-class UserStatus(models.TextChoices):
-    OFFLINE = "Offline"
-    ONLINE = "Online"
-    PLAYING = "Playing"
-
 class Users(models.Model):
     id = models.AutoField(primary_key=True)
     username = models.CharField(max_length=64)
@@ -33,13 +9,13 @@ class Users(models.Model):
     description = models.TextField()
     email = models.EmailField()
     picture = models.ImageField(default='default.jpg', upload_to='upload')
-    status = models.CharField(default=UserStatus.OFFLINE)
-    # created_at = models.DateTimeField(auto_now_add=True)
-    # update_at = models.DateTimeField(auto_now=True)
+    status = models.CharField(default='Offline')
+    created_at = models.DateTimeField(auto_now_add=True)
+    update_at = models.DateTimeField(auto_now=True)
 
     def save(self, *args, **kwargs):
         if self.status not in ['Offline', 'Online', 'Playing']:
-            raise ValidationError('Status must be one of Offline, Online or Playing')
+            raise ValidationError('Status must be one of "[\'Offline\', \'Online\'], \'Playing\']"')
         super().save(*args, **kwargs)
 
 class Friends(models.Model):
@@ -61,11 +37,19 @@ class UserStats(models.Model):
 
 class Notifications(models.Model):
     id = models.AutoField(primary_key=True)
-    type = models.CharField(choices=NotificationType.choices)
-    status = models.CharField(choices=NotificationStatus.choices, default=NotificationStatus.PENDING)
+    type = models.CharField()
+    status = models.CharField(default='Pending')
     description = models.CharField(max_length=255)
     user_id = models.ForeignKey(Users, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
+
+    def save(self, *args, **kwargs):
+        if self.status not in ['Read', 'Pending']:
+            raise ValidationError('Status must be one of "[\'Read\', \'Pending\']"')
+        if self.type not in ['Friend Request', 'Accepted Friend Request']:
+            raise ValidationError('Status must be one of "[\'Friend Request\', \'Accepted Friend Request\']"')
+
+        super().save(*args, **kwargs)
 
 class Games(models.Model):
     id = models.AutoField(primary_key=True)
@@ -82,17 +66,27 @@ class Tournaments(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=64)
     capacity = models.IntegerField()
-    status = models.CharField(choices=TournamentStatus.choices, default=TournamentStatus.OPEN)
+    status = models.CharField(default='Open')
     winner_id = models.ForeignKey(Users, related_name="tournament_winner", null=True, on_delete=models.SET_NULL)
     host_id = models.ForeignKey(Users, related_name="tournament_host", null=True, on_delete=models.SET_NULL)
     created_at = models.DateTimeField(auto_now_add=True)
     update_at = models.DateTimeField(auto_now=True)
 
+    
+    def save(self, *args, **kwargs):
+        if self.status not in ['Open', 'Ongoing', 'Finished']:
+            raise ValidationError('Status must be one of "[\'Open\', \'Ongoing\'], \'Finished\']"')
+        super().save(*args, **kwargs)
+
 class TournamentsGames(models.Model):
     id = models.AutoField(primary_key=True)
-    phase = models.CharField(choices=TournamentPhase.choices)
+    phase = models.CharField()
     tournament_id = models.ForeignKey(Tournaments, on_delete=models.CASCADE)
     game_id = models.ForeignKey(Games, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
 
+    def save(self, *args, **kwargs):
+        if self.phase not in ['Last 16', 'Quarter-final', 'Semi-final', 'Final']:
+            raise ValidationError('Status must be one of "[\'Last 16\', \'Quarter-final\'], \'Semi-final\'], \'Final\']"')
 
+        super().save(*args, **kwargs)
