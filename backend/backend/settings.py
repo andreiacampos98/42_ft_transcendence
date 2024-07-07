723535169a5ff3c686e52122b005ee5340293b6f
Andreia Campos <andreiacampos98.15@gmail.com>
2024-07-07 22:03:59 +0100

    import html pages and bootstrap

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    "pong"
+    "pong",
 ]
 
 MIDDLEWARE = [
@@ -120,6 +120,7 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 MEDIA_ROOT = BASE_DIR
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
