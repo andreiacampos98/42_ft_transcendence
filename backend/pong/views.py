bc699c98a165dd43bdec84ec1fdbed985b0de92b
Andreia Campos <andreiacampos98.15@gmail.com>
2024-07-07 21:38:24 +0100

    Code: imported our own HTML files to the django app.

--- backend/pong/views.py
+++ backend/pong/views.py
@@ -4,14 +4,14 @@ def base(request):
     """
     BASE PARA TEMPLATES
     """
-    return render(request, 'base.html')
+    return render(request, 'pages/home-view.html')
 
 def index(request):
     """
     Não necessita de uma verificação do tipo de requisição,
     pois retornaremos apenas um layout
     """
-    return render(request,"pages/index.html")
+    return render(request,"website/navs.html")
     
     
 def about(request):
@@ -19,7 +19,7 @@ def about(request):
     Não necessita de uma verificação do tipo de requisição,
     pois retornaremos apenas um layout
     """
-    return render(request,"pages/about.html")
+    return render(request,"website/pages/sign-up.html")
     
 def contact(request):
     """
@@ -28,4 +28,4 @@ def contact(request):
     um formulário, deve-se criar outra view com um template próprio
     para seu formulário.
     """
-    return render(request,"pages/contact.html")
+    return render(request,"website/pages/home-view.html")
