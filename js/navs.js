c7090920a6c94598190b0b648d8a4e76b32116cc
Andreia Campos <andreiacampos98.15@gmail.com>
2024-07-21 19:31:11 +0100

    remove duplicates

--- js/navs.js
+++
@@ -1,5 +0,0 @@
-
-function toggleLeftDiv() {
-    const leftDiv = document.getElementById('left-div');
-    leftDiv.classList.toggle('expanded');
-}
--- website/js/home_view.js
+++
@@ -1,9 +0,0 @@
-function toggleSidebar() {
-    const sidebar = document.getElementById('sidebar');
-    sidebar.classList.toggle('show');
-}
-
-function toggleLeftDiv() {
-    const leftDiv = document.getElementById('left-div');
-    leftDiv.classList.toggle('expanded');
-}
