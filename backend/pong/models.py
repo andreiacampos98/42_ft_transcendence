51444f5546988f6b0fe01a382975d45f9e30dacb
andreiacampos98 <andreiacampos98.15@gmail.com>
2024-07-29 20:32:45 +0100

    reset password not finished

--- backend/pong/models.py
+++ backend/pong/models.py
@@ -33,6 +33,7 @@ class Users(AbstractBaseUser, PermissionsMixin):
     status = models.CharField(max_length=7, default='Offline')
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
     is_superuser = models.BooleanField(default=False)
     last_login = models.DateTimeField(null=True, blank=True)
