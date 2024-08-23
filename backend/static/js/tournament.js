2ab613dc5fe9b4e7652f399c9dbc721211b51d79
Xyckens <franciscommvieira@hotmail.com.com>
2024-08-23 17:43:15 +0100

    tournament page

--- backend/static/js/tournament.js
+++ backend/static/js/tournament.js
@@ -1,4 +1,4 @@
-function setStatus(elementId, status) {
+/*function setStatus(elementId, status) {
     const friendBlock = document.getElementById(elementId);
     const statusSpan = friendBlock.querySelector('.status');
     
@@ -13,3 +13,26 @@ function setStatus(elementId, status) {
 setStatus('friend1', 'online');  // Can be 'online', 'offline', or 'playing'
 setStatus('friend2', 'offline');  // Can be 'online', 'offline', or 'playing'
 setStatus('friend3', 'playing');  // Can be 'online', 'offline', or 'playing'
+
+*/
+document.addEventListener('DOMContentLoaded', () => {
+    const statusElements = document.querySelectorAll('.status-tourn');
+
+    statusElements.forEach(statusElement => {
+        const statusText = statusElement.textContent.trim().toLowerCase();
+
+        switch (statusText) {
+            case 'ongoing':
+                statusElement.classList.add('status-ongoing');
+                break;
+            case 'open':
+                statusElement.classList.add('status-open');
+                break;
+            case 'closed':
+                statusElement.classList.add('status-closed');
+                break;
+            default:
+                break;
+        }
+    });
+});
