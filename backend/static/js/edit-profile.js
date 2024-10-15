b7cb3f15e3668a82d32282124974832de2cff50b
Nuno Jesus <nunomiguel533@gmail.com>
2024-10-15 21:44:55 +0100

    Fix + Code: fixed edit profile script being called twice and linked donut chart data to backend.

--- backend/static/js/edit-profile.js
+++ backend/static/js/edit-profile.js
@@ -86,130 +86,137 @@ window.onclick = function(event) {
     modal2.style.display = "none";
   }
 }
-/*
-const remoteTime = {{ stats.remote_time_played }};
-const aiTime = {{ stats.ai_time_played }};
-const localTime = {{ stats.local_time_played }};
-const tournamentTime = {{ stats.tournament_time_played }};
-*/
 
-const remoteTime = 11;
-const aiTime =38;
-const localTime =21;
-const tournamentTime =25;
-// ApexCharts Donut Chart Configuration
+async function getUserStats() {
+	const userID = document.querySelector('button[onclick="getChangePassword()"]').getAttribute('data-user-id');
+	const response = await fetch(`/stats/${userID}`, {
+		method: "GET",
+		// headers: {
+		// 	"X-CSRFToken": document.querySelector('[name=csrfmiddlewaretoken]').value
+		// }
+	});
+	const stats = await response.json();
+	console.log(stats);
+	
+	const remoteTime = Math.round(stats.remote_time_played / 60);
+	const aiTime = Math.round(stats.ai_time_played / 60);
+	const localTime = Math.round(stats.local_time_played / 60);
+	const tournamentTime = Math.round(stats.tournament_time_played / 60);
+
+	var options = {
+		chart: {
+			type: 'donut',       // Type of chart: Donut
+			height: 200          // Height of the chart
+		},
+		series: [remoteTime, aiTime, localTime, tournamentTime],  // Data values for the donut chart
+		labels: ['Remote Game', 'AI mode', 'Local Game', 'Tournaments'],  // Labels for each section
+		colors: ['#EC6158', '#46CDBD', '#66DD53', '#FFAD72'],     // Custom colors for each segment
+		legend: {
+			position: 'right',    // Legend position to the right
+			offsetY: 25,           // Center the legend vertically
+			offsetX: 100,
+			markers: {
+				width: 12,
+				height: 12
+			},
+			fontSize: '14px',
+			fontWeight: 'bold',
+			labels: {
+				colors: ['#fff','#fff','#fff','#fff']   // Change the legend text color to white
+			}
+		},
+		plotOptions: {
+			pie: {
+				donut: {
+					size: '70%',  // Size of the donut hole (70% of the total width)
+					labels: {
+						show: true,
+						total: {
+							show: true,
+							label: 'Min',
+							color: '#fff',
+							style: {
+								fontSize: '20px', // Font size of the number
+								fontWeight: 'bold',
+								color: '#fff'      // Color of the number
+							}
+						}
+					}
+				},
+				expandOnClick: false  // Prevent the slices from expanding when clicked
+			},
+		   
+		},
+		stroke: {
+			show: false,
+		
+		},
+		dataLabels: {
+			enabled: false         // Disable labels inside the slices
+		}
+	};
+	// Render the Donut Chart
+	var donut = new ApexCharts(document.querySelector("#chart1"), options);
+	donut.render();
+}
+
+getUserStats();
+
 var options = {
-    chart: {
-        type: 'donut',       // Type of chart: Donut
-        height: 200          // Height of the chart
-    },
-    series: [remoteTime, aiTime, localTime, tournamentTime],  // Data values for the donut chart
-    labels: ['Remote Game', 'AI mode', 'Local Game', 'Tournaments'],  // Labels for each section
-    colors: ['#EC6158', '#46CDBD', '#66DD53', '#FFAD72'],     // Custom colors for each segment
-    legend: {
-        position: 'right',    // Legend position to the right
-        offsetY: 25,           // Center the legend vertically
-        offsetX: 100,
-        markers: {
-            width: 12,
-            height: 12
-        },
-        fontSize: '14px',
-        fontWeight: 'bold',
-        labels: {
-            colors: ['#fff','#fff','#fff','#fff']   // Change the legend text color to white
-        }
-    },
-    plotOptions: {
-        pie: {
-            donut: {
-                size: '70%',  // Size of the donut hole (70% of the total width)
-                labels: {
-                    show: true,
-                    total: {
-                        show: true,
-                        label: 'Hours',
-                        color: '#fff',
-                        style: {
-                            fontSize: '20px', // Font size of the number
-                            fontWeight: 'bold',
-                            color: '#fff'      // Color of the number
-                        }
-                    }
-                }
-            },
-            expandOnClick: false  // Prevent the slices from expanding when clicked
-        },
-       
-    },
-    stroke: {
-        show: false,
-    
-    },
-    dataLabels: {
-        enabled: false         // Disable labels inside the slices
-    }
-};
-// Render the Donut Chart
-var donut = new ApexCharts(document.querySelector("#chart1"), options);
-donut.render();
-
-
-var options = {
-    chart: {
-        type: 'line', // We will use 'line' type to draw the connecting line
-        height: 350,
-        stacked: false, // Do not stack bars
-        toolbar: {
-            show: false // Hide the toolbar
-        },
-        width: '100%',
-    },
-    series: [{
-        name: 'Win Rate', // Added name for clarity
-        type: 'column', // Column series for bars
-        data: [7.0, 8.0, 9.0, 6.0, 8.5, 7.5, 9.5] // Sample win rates for each day
-    }, {
-        name: 'Win Rate Line', // Added name for clarity
-        type: 'line', // Line series to connect the bars
-        data: [7.0, 8.0, 9.0, 6.0, 8.5, 7.5, 9.5], // Same data to connect the tops
-        stroke: {
-            width: 2,
-        },
-    }],
-    xaxis: {
-        categories: ['Mon', 'Tues', 'Wed', 'Thur', 'Fri', 'Sat', 'Sun'], // Days of the week
-        labels: {
-            rotate: 0, // Set labels to horizontal
-            style: {
-                colors: '#c3c3c3bb',  // Color of x-axis labels
-                fontSize: '14px',   // Font size of x-axis labels
-                fontWeight: 600     // Font weight (boldness)
-            }
-        },
-        axisBorder: {
-            show: false // Hide x-axis border
-        },
-        axisTicks: {
-            show: false // Hide ticks on the x-axis
-        }
-    },
-    colors: ['#605CFF', '#83E9FF'], // Colors for the bars and line
-    yaxis: {
-        show: false // Hide the y-axis
-    },
-    grid: {
-        show: false // Hide the grid
-    },
-    tooltip: {
-        enabled: false // Disable tooltips
-    },
-    dataLabels: {
-        enabled: false // Disable data labels
-    },
-    legend: {
-        show: false
-      }
+	chart: {
+		type: 'line', // We will use 'line' type to draw the connecting line
+		height: 350,
+		stacked: false, // Do not stack bars
+		toolbar: {
+			show: false // Hide the toolbar
+		},
+		width: '100%',
+	},
+	series: [{
+		name: 'Win Rate', // Added name for clarity
+		type: 'column', // Column series for bars
+		data: [7.0, 8.0, 9.0, 6.0, 8.5, 7.5, 9.5] // Sample win rates for each day
+	}, {
+		name: 'Win Rate Line', // Added name for clarity
+		type: 'line', // Line series to connect the bars
+		data: [7.0, 8.0, 9.0, 6.0, 8.5, 7.5, 9.5], // Same data to connect the tops
+		stroke: {
+			width: 2,
+		},
+	}],
+	xaxis: {
+		categories: ['Mon', 'Tues', 'Wed', 'Thur', 'Fri', 'Sat', 'Sun'], // Days of the week
+		labels: {
+			rotate: 0, // Set labels to horizontal
+			style: {
+				colors: '#c3c3c3bb',  // Color of x-axis labels
+				fontSize: '14px',   // Font size of x-axis labels
+				fontWeight: 600     // Font weight (boldness)
+			}
+		},
+		axisBorder: {
+			show: false // Hide x-axis border
+		},
+		axisTicks: {
+			show: false // Hide ticks on the x-axis
+		}
+	},
+	colors: ['#605CFF', '#83E9FF'], // Colors for the bars and line
+	yaxis: {
+		show: false // Hide the y-axis
+	},
+	grid: {
+		show: false // Hide the grid
+	},
+	tooltip: {
+		enabled: false // Disable tooltips
+	},
+	dataLabels: {
+		enabled: false // Disable data labels
+	},
+	legend: {
+		show: false
+	  }
 };
 
 var columns = new ApexCharts(document.querySelector("#chart2"), options);
