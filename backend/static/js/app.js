0193f29958dcaa47f048d9d592fcbe0e7ca0dce7
Nuno Jesus <nunomiguel533@gmail.com>
2024-10-26 00:36:49 +0100

    Code: explored how to fix tournament script overall. Changed tournament status logic to HTMX.

--- backend/static/js/app.js
+++ backend/static/js/app.js
@@ -1,17 +1,32 @@
-const scrollContainer_meu = document.querySelector('.main');
+// const scrollContainer_meu = document.querySelector('.main');
 
-scrollContainer_meu.addEventListener('wheel', (event) => {
-    // Check if the content requires horizontal scrolling
-    const canScrollHorizontally = scrollContainer_meu.scrollWidth > scrollContainer_meu.clientWidth;
-    // Check if the content requires vertical scrolling
-    const canScrollVertically = scrollContainer_meu.scrollHeight > scrollContainer_meu.clientHeight;
+// scrollContainer_meu.addEventListener('wheel', (event) => {
+//     // Check if the content requires horizontal scrolling
+//     const canScrollHorizontally = scrollContainer_meu.scrollWidth > scrollContainer_meu.clientWidth;
+//     // Check if the content requires vertical scrolling
+//     const canScrollVertically = scrollContainer_meu.scrollHeight > scrollContainer_meu.clientHeight;
 
-    if (canScrollHorizontally && !event.shiftKey) {
-        // If horizontal scrolling is needed and Shift is not held down, scroll horizontally
-        event.preventDefault(); // Prevent vertical scrolling
-        scrollContainer_meu.scrollLeft += event.deltaY; // Scroll horizontally by deltaY amount
-    } else if (canScrollVertically && (event.shiftKey || !canScrollHorizontally)) {
-        // If vertical scrolling is needed and Shift is held down or horizontal scrolling isn't needed
-        scrollContainer_meu.scrollTop += event.deltaY; // Scroll vertically
-    }
-});
+//     if (canScrollHorizontally && !event.shiftKey) {
+//         // If horizontal scrolling is needed and Shift is not held down, scroll horizontally
+//         event.preventDefault(); // Prevent vertical scrolling
+//         scrollContainer_meu.scrollLeft += event.deltaY; // Scroll horizontally by deltaY amount
+//     } else if (canScrollVertically && (event.shiftKey || !canScrollHorizontally)) {
+//         // If vertical scrolling is needed and Shift is held down or horizontal scrolling isn't needed
+//         scrollContainer_meu.scrollTop += event.deltaY; // Scroll vertically
+//     }
+// });
+
+const callback = (mutations) => {
+	// console.log(mutations);
+	mutations.forEach((mutation) => {
+		mutation.addedNodes.forEach((node) => {
+			// if (node.nodeType === 1 && !node["htmx-internal-data"]) {
+				htmx.process(node);
+			// }
+		})
+	});
+};
+
+const observer = new MutationObserver(callback);
+
+observer.observe(document, {childList: true, subtree: true});
--- backend/static/js/tournament.js
+++ backend/static/js/tournament.js
@@ -1,132 +1,95 @@
+function initModal(){
+	let modal = document.getElementById("modal2");
+	let openModalButton = document.getElementById("tournament-creater");
+	let goBackButton = document.getElementById("cancel-create-tournament");
 
-document.addEventListener('DOMContentLoaded', () => {
-    const observer = new MutationObserver((mutations) => {
-        mutations.forEach((mutation) => {
-          mutation.addedNodes.forEach((node) => {
-            if (node.nodeType === 1 && !node["htmx-internal-data"]) {
-              htmx.process(node)
-            }
-          })
-        })
-      });
-    observer.observe(document, {childList: true, subtree: true});
-})
+	openModalButton.onclick = () => modal.style.display = "block";
+	goBackButton.onclick = () => modal.style.display = "none";
+	window.onclick = (event) => {
+		if (event.target == modal)
+			modal.style.display = "none";
+	}
 
-htmx.onLoad(() => {
-    const statusElements = document.querySelectorAll('.status-tourn');
+	let checkbox = document.getElementById('use-username-checkbox');
+	let nicknameInput = document.getElementById('nickname-input-create');
 
-    statusElements.forEach(statusElement => {
-        const statusText = statusElement.textContent.trim().toLowerCase();
+	checkbox.addEventListener('change', function() {
+		if (this.checked) {
+			nicknameInput.disabled = true;  
+			nicknameInput.placeholder = "Using username";
+		} else {
+			nicknameInput.disabled = false; 
+			nicknameInput.placeholder = "Insert your nickname here";
+		}
+	});
+};
 
-        switch (statusText) {
-            case 'ongoing':
-                statusElement.classList.add('status-ongoing');
-                break;
-            case 'open':
-                statusElement.classList.add('status-open');
-                break;
-            case 'closed':
-                statusElement.classList.add('status-closed');
-                break;
-            default:
-                break;
-        }
-    });
-});
-
-// Get the modal
-var modal = document.getElementById("modal2");
-// Get the button that opens the modal
-var btn = document.getElementById("tournament-creater");
-// Get the  element that closes the modal
-
-var goback = document.getElementById("cancel-create-tournament");
-// When the user clicks the button, open the modal 
-btn.onclick = function() {
-  modal.style.display = "block";
-}
-
-goback.onclick = function() {
-  modal.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-}
-
-const createTournamentButton = document.getElementById('tournament-creater');
-createTournamentButton.onmouseenter = function () {
-	document.getElementById('plus-icon').src = "/static/assets/icons/plus-hover.png";
-}
-createTournamentButton.onmouseleave = function () {
-	document.getElementById('plus-icon').src = "/static/assets/icons/plus.png";
-}
+function initCreateTournamentButton(){
+	const createTournamentButton = document.getElementById('tournament-creater');
+	createTournamentButton.onmouseenter = function () {
+		document.getElementById('plus-icon').src = "/static/assets/icons/plus-hover.png";
+	}
+	createTournamentButton.onmouseleave = function () {
+		document.getElementById('plus-icon').src = "/static/assets/icons/plus.png";
+	}
+};
 
 function onCreateButtonClick()
 {
-    const userId = document.querySelector('button[onclick="onCreateButtonClick()"]').getAttribute('data-user-id');
-    const checkbox = document.getElementById('use-username-checkbox');
-    var alias;
-    if(checkbox.checked)
-    {
-      alias = document.getElementById("nickname-input-create").getAttribute('data-user-username');
-    } else {
-      alias = document.getElementById("nickname-input-create").value;
-    }
-    var formData = {
-        "name": document.getElementById("new-tournament-name").value,
-        "capacity":  document.getElementById("numPlayers").value,
-        "host_id": userId,
-        "alias": alias,
-        "status": 'Open'
-    };
-    console.log(formData)
+	const userId = document.querySelector('button[onclick="onCreateButtonClick()"]').getAttribute('data-user-id');
+	const checkbox = document.getElementById('use-username-checkbox');
+	var alias;
 
-    fetch(`/tournaments/create`, {
-        method: "POST",
-        body: JSON.stringify(formData),
-        headers: {
-            'Content-Type': 'application/json',
-            'X-CSRFToken': document.querySelector('[name=csrfmiddlewaretoken]').value
-        }
-    })
-    .then(response => response.json())
-    .then(data => {
-      if (JSON.stringify(data.data) === '{}') {
-          alert(data.message);
-      } else {
-          alert("Tournament created successfully!");
-          const tournamentId = data.data.id; // Ajuste conforme o formato da resposta
-          console.log(data.data);
-          localStorage.setItem('alias', formData.alias);
-          localStorage.setItem('tournament_id', tournamentId);
-          history.pushState(null, '', `/tournaments/ongoing/${tournamentId}`);
-          htmx.ajax('GET', `/tournaments/ongoing/${tournamentId}`, {
-              target: '#main' , 
-          });
-      }
-    })
-    .then(data => {
-        if (data.data != {}) {
-        } else {
-            alert("Error: " + (data.message || 'Unknown error'));
-        }
-    })
-    .catch(error => console.error('Error:', error));
+	if (checkbox.checked)
+		alias = document.getElementById("nickname-input-create").getAttribute('data-user-username');
+	else 
+		alias = document.getElementById("nickname-input-create").value;
+	
+	var formData = {
+		"name": document.getElementById("new-tournament-name").value,
+		"capacity":  document.getElementById("numPlayers").value,
+		"host_id": userId,
+		"alias": alias,
+		"status": 'Open'
+	};
+	console.log(formData)
+
+	fetch(`/tournaments/create`, {
+		method: "POST",
+		body: JSON.stringify(formData),
+		headers: {
+			'Content-Type': 'application/json',
+			'X-CSRFToken': document.querySelector('[name=csrfmiddlewaretoken]').value
+		}
+	})
+	.then(response => response.json())
+	.then(data => {
+		if (JSON.stringify(data.data) === '{}') {
+			alert(data.message);
+		} else {
+			alert("Tournament created successfully!");
+			const tournamentId = data.data.id; // Ajuste conforme o formato da resposta
+			console.log(data.data);
+			localStorage.setItem('alias', formData.alias);
+			localStorage.setItem('tournament_id', tournamentId);
+			history.pushState(null, '', `/tournaments/ongoing/${tournamentId}`);
+			htmx.ajax('GET', `/tournaments/ongoing/${tournamentId}`, {
+				target: '#main' , 
+			});
+		}
+	})
+	.then(data => {
+		if (data.data != {}) {
+
+		} else {
+			alert("Error: " + (data.message || 'Unknown error'));
+		}
+	})
+	.catch(error => console.error('Error:', error));
 }
 
-var checkbox = document.getElementById('use-username-checkbox');
-var nicknameInput = document.getElementById('nickname-input-create');
-
-checkbox.addEventListener('change', function() {
-    if (this.checked) {
-        nicknameInput.disabled = true;  
-        nicknameInput.placeholder = "Using username";
-    } else {
-        nicknameInput.disabled = false; 
-        nicknameInput.placeholder = "Insert your nickname here";
-    }
+window.addEventListener('htmx:beforeRequest', (event) => {
+	console.log('HERE');
+	initModal();
+	initCreateTournamentButton();
 });
