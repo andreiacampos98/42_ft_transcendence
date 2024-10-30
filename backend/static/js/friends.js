e42f7971c971e8bdec45c5d1fd6fd64283414154
Nuno Jesus <nunomiguel533@gmail.com>
2024-10-30 13:23:31 +0000

    Refactoring: added async to fetch on get user friends.

--- backend/static/js/friends.js
+++ backend/static/js/friends.js
@@ -23,7 +23,7 @@ function addClassToTopLevelDivs(className) {
     });
 }
 
-function toggleSidebar(user_id) {
+async function toggleFriendsDrawer(user_id) {
 
     const sidebar = document.getElementById('sidebar');
     sidebar.classList.toggle('show');
@@ -34,72 +34,69 @@ function toggleSidebar(user_id) {
     if (!sidebar.classList.contains('show'))
         return ;
 
-    fetch(`/friends/${user_id}`, {
+    const response = await fetch(`/friends/${user_id}`, {
         headers: {
             'X-Requested-With': 'XMLHttpRequest'
         }
-    })
-    .then(response => response.json())
-    .then(entries => {
-        var friends = entries.map((entry) => {
-            if (entry.user1_id.id == user_id)
-                return entry.user2_id
-            else
-                return entry.user1_id
-        });
-
-        const sidebar = document.getElementById('sidebar');
-        sidebar.innerHTML = '';  // Clear existing content
-
-        friends.forEach((friend) => {
-            console.log(friend);
-
-            const friendBlock = document.createElement('div');
-            friendBlock.classList.add('friend-block');
-            friendBlock.id = 'friend2';
-            friendBlock.dataset.status = 'online';
-            
-            const link = document.createElement('a');
-            link.style.display = 'block'; 
-
-            const profilePic = document.createElement('img');
-            profilePic.classList.add('profile-pic');
-            const uri = friend.picture;
-            if (friend.picture.includes('http')) {
-                profilePic.src = decodeURIComponent(uri.slice(7))
-            } else {
-                profilePic.src = friend.picture;
-            }
-            profilePic.alt = 'Profile'; 
-
-            const friendInfo = document.createElement('div');
-            friendInfo.classList.add('friend-info');
-
-            const username = document.createElement('h4');
-            username.textContent = friend.username;
-
-            const status = document.createElement('span');
-            status.classList.add('friend-block1');
-            status.setAttribute('data-status', friend.status.toLowerCase());
-            status.textContent = friend.status;
-
-            friendBlock.onclick = function() {
-                history.pushState(null, '', `/users/${friend.id}`);
-                htmx.ajax('GET', `/users/${friend.id}`, {
-                    target: '#main'
-                });
-            };
-
-            friendInfo.appendChild(username);
-            friendInfo.appendChild(status);
-            friendBlock.appendChild(profilePic);
-            friendBlock.appendChild(friendInfo);
-            link.appendChild(friendBlock);
-            sidebar.appendChild(link);
-
-
-        });
     });
-}
+	const data = await response.json();
 
+    var friends = data.map((entry) => {
+		if (entry.user1_id.id == user_id)
+			return entry.user2_id;
+		else
+			return entry.user1_id;
+	});
+
+	sidebar.innerHTML = '';  // Clear existing content
+
+	friends.forEach((friend) => {
+		console.log(friend);
+
+		const friendBlock = document.createElement('div');
+		friendBlock.classList.add('friend-block');
+		friendBlock.id = 'friend2';
+		friendBlock.dataset.status = 'online';
+		
+		const link = document.createElement('a');
+		link.style.display = 'block'; 
+
+		const profilePic = document.createElement('img');
+		profilePic.classList.add('profile-pic');
+		profilePic.alt = 'Profile'; 
+
+		const uri = friend.picture;
+		if (friend.picture.includes('http')) 
+			profilePic.src = decodeURIComponent(uri.slice(7))
+		else 
+			profilePic.src = friend.picture;
+
+		const friendInfo = document.createElement('div');
+		friendInfo.classList.add('friend-info');
+
+		const username = document.createElement('h4');
+		username.textContent = friend.username;
+
+		const status = document.createElement('span');
+		status.classList.add('friend-block1');
+		status.setAttribute('data-status', friend.status.toLowerCase());
+		status.textContent = friend.status;
+
+		friendBlock.onclick = function() {
+			history.pushState(null, '', `/users/${friend.id}`);
+			htmx.ajax('GET', `/users/${friend.id}`, {
+				target: '#main'
+			});
+		};
+
+		friendInfo.appendChild(username);
+		friendInfo.appendChild(status);
+		friendBlock.appendChild(profilePic);
+		friendBlock.appendChild(friendInfo);
+		link.appendChild(friendBlock);
+		sidebar.appendChild(link);
+
+
+	});
+}
     
