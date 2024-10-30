de2cf74ede79416616ef196f86916271a98f18ff
Nuno Jesus <nunomiguel533@gmail.com>
2024-10-30 13:16:52 +0000

    Refactoring: added async to fetch on add/remove friends.

--- backend/static/js/friends_add_remove.js
+++ backend/static/js/friends_add_remove.js
@@ -1,7 +1,7 @@
-function friends_add(event, userId1, userId2) {
+async function friends_add(event, userId1, userId2) {
     event.preventDefault(); 
 
-    fetch(`/friends/${userId1}/${userId2}`, {
+    const response = await fetch(`/friends/${userId1}/${userId2}`, {
         method: 'POST',
         headers: {
             'X-Requested-With': 'XMLHttpRequest',
@@ -12,44 +12,33 @@ function friends_add(event, userId1, userId2) {
             'user1_id': userId1,
             'user2_id': userId2
         })
-    })
-    .then(response => response.json())
-    .then(data => {
-        if (JSON.stringify(data.data) === '{}') {
-            alert(data.message);
-        } else {
-            alert(data.message);
-            window.location.reload();
-            // Optionally, you might want to update the UI here
-        }
-    })
-    .catch(error => console.error('Error:', error));
+    });
 
-    return false; // Prevent form submission
+	const data = await response.json();
+	if (!response.ok)
+		alert(data.message)
+	else
+		window.location.reload();
+		
+    return false;
 }
 
-function friends_remove(event, userId1, userId2) {
+async function friends_remove(event, userId1, userId2) {
     event.preventDefault(); 
-    fetch(`/friends/${userId1}/${userId2}`, {
+    const response = await fetch(`/friends/${userId1}/${userId2}`, {
         method: 'DELETE',
         headers: {
             'X-Requested-With': 'XMLHttpRequest',
             'Content-Type': 'application/json',
             'X-CSRFToken': document.querySelector('[name=csrfmiddlewaretoken]').value
         }
-    })
-    .then(response => response.json())
-    .then(data => {
-        if (JSON.stringify(data.data) === '{}') {
-            alert(data.message);
-        } else {
-            alert(data.message);
-            window.location.reload();
-            // Optionally, you might want to update the UI here
-            // For example, you might remove the friend from the UI or refresh the friend list
-        }
-    })
-    .catch(error => console.error('Error:', error));
+    });
 
-    return false; // Prevent form submission
+	const data = await response.json();
+    if (!response.ok)
+		alert(data.message);
+	else
+		window.location.reload();
+	
+    return false;
 }
