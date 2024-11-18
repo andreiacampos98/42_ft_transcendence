219d29054beaaa255c771fb36bda727413e5522e
Nuno Jesus <nunomiguel533@gmail.com>
2024-11-18 11:28:51 +0000

    Code: added a safe mechanism to update a remote game if even if one disconnects.

--- backend/pong/consumers/RemoteGame.py
+++ backend/pong/consumers/RemoteGame.py
@@ -14,16 +14,22 @@ class RemoteGameQueueConsumer(AsyncWebsocketConsumer):
 		await self.accept()
 		self.user = self.scope['user']
 		self.redis = get_redis_connection('default')
-		self.game_channel = f'room_of_host_{self.user.id}'
+		self.game_channel = ''
+		self.has_opponent_disconnected = False
 
 		await self.queue_up()
+		for key in self.get_game_queue():
+			ic(key, self.get_waiting_room(key))
 	
 	async def disconnect(self, code):
-		# if self.user.id in self.queue:
-		# 	del self.queue[self.user.id]
-		self.hashmap_del_cache(self.user.id)
-		for key in self.hashmap_getall_cache():
-			ic(key, self.hashmap_get_cache(key))
+		self.del_waiting_room(self.user.id)
+		if self.game_channel:
+			await self.channel_layer.group_send( self.game_channel, {
+				"type": "signal.disconnection",
+				"message": {}
+			})
+		for key in self.get_game_queue():
+			ic(key, self.get_waiting_room(key))
 		return await super().disconnect(code)
 	
 	async def receive(self, text_data=None):
@@ -42,29 +48,61 @@ class RemoteGameQueueConsumer(AsyncWebsocketConsumer):
 
 	async def broadcast(self, event):
 		await self.send(event['message'])
-
+	
+	async def broadcast_and_set_channel(self, event):
+		data = json.loads(event['message'])
+		self.game_channel = f'remote_game_{data['gameID']}'
+		await self.send(event['message'])
+	
+	async def signal_disconnection(self, event):
+		self.has_opponent_disconnected = True
+		
 	# ! ============================= REDIS ACCESS =============================
 	
-	def hashmap_getall_cache(self):
+	def get_game_queue(self):
 		return [json.loads(key.decode('UTF-8')) for key in self.redis.hgetall('remote_game_queue')]
 
-	def hashmap_get_cache(self, key):
+	def get_waiting_room(self, key):
 		return json.loads(self.redis.hget('remote_game_queue', key).decode('UTF-8'))
 
-	def hashmap_set_cache(self, key, value):
+	def set_waiting_room(self, key, value):
 		return self.redis.hset('remote_game_queue', key, json.dumps(value))
 
-	def hashmap_del_cache(self, key):
+	def del_waiting_room(self, key):
 		return self.redis.hdel('remote_game_queue', key)
+	
+	def get_game_results(self, key):
+		data = self.redis.hget('remote_game_results', key)
+		return json.loads(data.decode('UTF-8')) if data is not None else None
+	
+	def set_game_results(self, key, value):
+		return self.redis.hset('remote_game_results', key, json.dumps(value))
+
+	def del_game_results(self, key):
+		return self.redis.hdel('remote_game_results', key)
 
 	# ! ============================= DATABASE ACCESS ==========================
 
 	@database_sync_to_async
 	def on_game_end(self, data):
 		game_id = data['id']
-		del data['id']
-		game_update_helper(data['data'], game_id)
-	
+		game_results = self.get_game_results(game_id)
+		
+		if game_results is not None:
+			ic(f'Data for game {game_id} already exists, {self.user.username}')
+			ic(game_results)
+			game_update_helper(game_results, game_id)
+			self.del_game_results(game_id)
+		else:
+			ic(f'Storing game {game_id} with {self.user.username} data...')
+			del data['id']
+			self.set_game_results(game_id, data)
+
+		if game_results is None and self.has_opponent_disconnected:
+			ic(f'Opponent of {self.user.username} disc. Updating game {game_id}')
+			game_update_helper(data, game_id)
+			self.del_game_results(game_id)
+		
 	@database_sync_to_async
 	def create_new_game(self, host_id):
 		new_game_data = {
@@ -84,9 +122,9 @@ class RemoteGameQueueConsumer(AsyncWebsocketConsumer):
 		(a 'waiting room') to allow another player to join in
 		"""
 
-		available_rooms = self.hashmap_getall_cache()
+		available_rooms = self.get_game_queue()
 		for key in available_rooms:
-			ic(key, self.hashmap_get_cache(key))
+			ic(key, self.get_waiting_room(key))
 		if self.user.id in available_rooms:
 			ic(f'{self.user.id} - {self.user.username} already in the waiting rooms')
 			return
@@ -97,10 +135,9 @@ class RemoteGameQueueConsumer(AsyncWebsocketConsumer):
 			user_data = {
 				'id': self.user.id,
 				'username': self.scope['user'].username,
-				'game_channel': self.game_channel,
 				'channel_name': self.channel_name
 			}
-			self.hashmap_set_cache(self.user.id, user_data)
+			self.set_waiting_room(self.user.id, user_data)
 
 
 	async def pair_player(self, host_room_id):
@@ -114,17 +151,17 @@ class RemoteGameQueueConsumer(AsyncWebsocketConsumer):
 		curr_player = {
 			'id': self.user.id,
 			'username': self.scope['user'].username,
-			'game_channel': self.game_channel
 		}
-		host_player = self.hashmap_get_cache(host_room_id)
+		host_player = self.get_waiting_room(host_room_id)
 		game = await self.create_new_game(host_player['id'])
 
-		self.game_channel = host_player['game_channel']
+		# ! I HAVE TO CHANGE THE GAME CHANNEL OF BOTH HOST AND CLIENT
+		self.game_channel = f'remote_game_{game['id']}'
+		self.game_id = game['id']
 
 		await self.channel_layer.group_add(self.game_channel, self.channel_name)
 		await self.channel_layer.group_add(self.game_channel, host_player['channel_name'])
-		await self.channel_layer.group_send(self.game_channel, {
-			"type": "broadcast", 
+		await self.channel_layer.group_send(self.game_channel, {"type": "broadcast.and.set.channel", 
 			"message": json.dumps({
 				'gameID': game['id'],
 				'player1': host_player,
@@ -137,4 +174,4 @@ class RemoteGameQueueConsumer(AsyncWebsocketConsumer):
 				}
 			})
 		})
-		self.hashmap_del_cache(host_player['id'])
+		self.del_waiting_room(host_player['id'])
