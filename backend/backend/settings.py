eb84d0501513ba7eb367077a694243451f769bbe
Nuno Jesus <93390807+Nuno-Jesus@users.noreply.github.com>
2024-12-22 19:17:00 +0000

    Update settings.py

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -219,7 +219,6 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 EMAIL_HOST_USER = os.getenv("EMAIL_USER")
-EMAIL_HOST_PASSWORD = os.getenv("EMAIL_PASS")
 
 
 # Default primary key field type
